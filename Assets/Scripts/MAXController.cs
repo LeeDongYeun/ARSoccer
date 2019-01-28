c60b423a0776a3c95e0d600ca5a03c2c9e343c96
Jungwoo Kim <jw950310@kaist.ac.kr>
2019-01-29 05:11:58 +0900

    Add action with mouse clicks
    Left : punch
    Right : kick
    Wheel : death

--- Assets/Scripts/MAXController.cs
+++ Assets/Scripts/MAXController.cs
@@ -14,11 +14,41 @@ public class MAXController : NetworkBehaviour
         {
             return;
         }
-        //if(Input.GetKey(KeyCode.S)==true)
-        //{
-        //    transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            GetComponent<Animator>().SetBool("Jumpchk", true);
+            //transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) == true)
+        {
+            GetComponent<Animator>().SetBool("Jumpchk", false);
+            //transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Animator>().SetBool("Punchchk", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            GetComponent<Animator>().SetBool("Punchchk", false);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            GetComponent<Animator>().SetBool("Kickchk", true);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            GetComponent<Animator>().SetBool("Kickchk", false);
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            GetComponent<Animator>().SetBool("Eggchk", true);
+        }
+        if (Input.GetMouseButtonUp(2))
+        {
+            GetComponent<Animator>().SetBool("Eggchk", false);
+        }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
         Debug.Log(x + " and " + z);
