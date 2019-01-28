d27fe4c5603de42af95dd31eda749c3279ff1529
KimJungwoo <jw950310@kaist.ac.kr>
2019-01-29 01:31:24 +0900

    initt

---
+++ Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour
+{
+    [SerializeField] Camera FPSCharacterCam;
+    [SerializeField] AudioListener audioListener;
+
+    public override void OnStartLocalPlayer()
+    {
+        
+    }
+
+}
