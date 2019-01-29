27a640028e033daf9584e64e65d445c61dd0c485
LeeDongYeun <ledoye@kaist.ac.kr>
2019-01-29 19:14:25 +0900

    adogen

--- Assets/Scripts/MAXController.cs
+++ Assets/Scripts/MAXController.cs
@@ -7,6 +7,12 @@ using UnityStandardAssets.CrossPlatformInput;
 public class MAXController : NetworkBehaviour
 {
     public float moveSpeed = 2f;
+
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawn;
+    public Transform bulletSpawn2;
+    public Transform bulletSpawn3;
     // Update is called once per frame
     void Update()
     {
@@ -14,14 +20,17 @@ public class MAXController : NetworkBehaviour
         {
             return;
         }
+        /*
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
             GetComponent<Animator>().SetBool("Jumpchk", true);
             //transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        }
+        }*/
         if (Input.GetKeyUp(KeyCode.Space) == true)
         {
-            GetComponent<Animator>().SetBool("Jumpchk", false);
+            GetComponent<Animator>().SetBool("Punchchk", false);
+            CmdFire();
+            //GetComponent<Animator>().SetBool("Jumpchk", false);
             //transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
 
@@ -36,10 +45,14 @@ public class MAXController : NetworkBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             GetComponent<Animator>().SetBool("Kickchk", true);
+            Invoke("CmdFires", 1.5f);
+            
+            //CmdFires();
         }
         if (Input.GetMouseButtonUp(1))
         {
             GetComponent<Animator>().SetBool("Kickchk", false);
+            
         }
         if (Input.GetMouseButtonDown(2))
         {
@@ -63,5 +76,46 @@ public class MAXController : NetworkBehaviour
         float animSpeed = Mathf.Abs(vertical);
         GetComponent<Animator>().SetFloat("Speed", animSpeed);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Animator>().SetBool("Punchchk", true);
+            
+            
+        }
+
     }
+
+    [Command]
+    void CmdFire()
+    {
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 4.0f);
+    }
+
+    [Command]
+    void CmdFires()
+    {
+        
+
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 4.0f);
+
+        var bullet2 = (GameObject)Instantiate(bulletPrefab, bulletSpawn2.position, bulletSpawn2.rotation);
+        bullet2.GetComponent<Rigidbody>().velocity = bullet2.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet2);
+        Destroy(bullet2, 4.0f);
+        var bullet3 = (GameObject)Instantiate(bulletPrefab, bulletSpawn3.position, bulletSpawn3.rotation);
+        bullet3.GetComponent<Rigidbody>().velocity = bullet3.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet3);
+        Destroy(bullet3, 4.0f);
+    }
+
 }
